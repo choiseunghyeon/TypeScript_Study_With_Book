d84365106d8e41d4176919c46802724bee89854c
ket87 <ket8780@gmail.com>
2020-10-31 02:15:53 +0900

    finished p.44

---
+++ ch02-1/dist/utils/makePerson.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.testMakePerson = exports.makePerson = void 0;
+function makePerson(name, age) {
+    return { name: name, age: age };
+}
+exports.makePerson = makePerson;
+function testMakePerson() {
+    console.log(makePerson('Jane', 22), makePerson('Jack', 33));
+}
+exports.testMakePerson = testMakePerson;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFrZVBlcnNvbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy91dGlscy9tYWtlUGVyc29uLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLFNBQWdCLFVBQVUsQ0FBQyxJQUFZLEVBQUUsR0FBVztJQUNoRCxPQUFPLEVBQUMsSUFBSSxNQUFBLEVBQUUsR0FBRyxLQUFBLEVBQUMsQ0FBQztBQUN2QixDQUFDO0FBRkQsZ0NBRUM7QUFFRCxTQUFnQixjQUFjO0lBQzFCLE9BQU8sQ0FBQyxHQUFHLENBQ1AsVUFBVSxDQUFDLE1BQU0sRUFBRSxFQUFFLENBQUMsRUFDdEIsVUFBVSxDQUFDLE1BQU0sRUFBRSxFQUFFLENBQUMsQ0FDekIsQ0FBQztBQUVOLENBQUM7QUFORCx3Q0FNQyJ9
